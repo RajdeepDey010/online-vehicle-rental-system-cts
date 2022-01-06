e66b5152e6d48d8bde159df54fbc970c0072da26
hp <hp@DESKTOP-0MC5R06>
2022-01-06 13:59:02 +0530

    Initial commit of the vehicle management application.

---
+++ src/main/java/com/cts/OnlineVehicleRentalApplication.java
@@ -0,0 +1,13 @@
+package com.cts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineVehicleRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineVehicleRentalApplication.class, args);
+	}
+
+}
