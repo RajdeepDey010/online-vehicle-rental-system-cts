0ff86354ce4990e27f552d75ffe6c3ec89066f52
2222883 <Srinjoy.Chaudhuri@cognizant.com>
2023-05-20 12:45:59 +0530

    UserDetails validation Issue #5 resolved

--- src/main/java/com/cts/model/UserDetails.java
+++ src/main/java/com/cts/model/UserDetails.java
@@ -2,7 +2,7 @@ package com.cts.model;
 
 import com.cts.enums.UserType;
 import lombok.*;
-
+/*
 @Getter
 @Setter
 @Builder
@@ -15,4 +15,34 @@ public class UserDetails {
     private String address;
     private boolean blocked;
     private UserType userType;
+}
+*/
+
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+public class UserDetails {
+    @Pattern(regexp = "^[\\w\\s-]+$", message = "License number must contain only numbers, alphabets, spaces, and '-' symbol")
+    private String licenseNumber;
+
+    @Pattern(regexp = "^\\d{2}-\\d{10}$", message = "Phone number must be in countrycode-mobilenumber format (e.g., 91-8876522421)")
+    private String phoneNumber;
+
+    // Getters and setters
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
