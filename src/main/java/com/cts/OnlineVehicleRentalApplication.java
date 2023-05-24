b2390b4904c9dca48c481eb050b5de07eda3cb02
2222813 <poshala.saideepak@cognizant.com>
2023-05-24 12:02:42 +0530

    Application starting on port given by user, Issue #3 resolved

--- src/main/java/com/cts/OnlineVehicleRentalApplication.java
+++ src/main/java/com/cts/OnlineVehicleRentalApplication.java
@@ -1,18 +1,27 @@
 package com.cts;
 
-import com.cts.port.YourApplication;
+import java.util.Scanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OnlineVehicleRentalApplication {
-
+public class OnlineVehicleRentalApplication extends SpringBootServletInitializer {
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(OnlineVehicleRentalApplication.class);
+    }
+    
     public static void main(String[] args) {
+    	
+    	Scanner sc = new Scanner(System.in);
+        if (args.length > 0) {
+        	System.out.println("Enter a PORT: ");
+            int port = sc.nextInt();
+            System.setProperty("server.port", String.valueOf(port));
+        }
         SpringApplication.run(OnlineVehicleRentalApplication.class, args);
-        SpringApplication.run(YourApplication.class, args);
     }
 }
-
-
-
-
