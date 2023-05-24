5078b5c6916059f1a83a6a341da78733b250b618
2222677 <jayant.banala@cognizant.com>
2023-05-24 12:24:07 +0530

    phone number and licence number validations done, Issue #2 resolved

--- src/main/java/com/cts/model/VehicleBook.java
+++ src/main/java/com/cts/model/VehicleBook.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Date;
+import java.util.regex.Pattern;
 
 @Getter
 @Setter
@@ -17,6 +18,40 @@ public class VehicleBook {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
     private Date endDuration;
     private String bookedByUser;
+
+    // Regular expression pattern for validating license number
+    private static final String LICENSE_NUMBER_PATTERN = "^[\\w\\s-]+$";
+
+    // Regular expression pattern for validating phone number
+    private static final String PHONE_NUMBER_PATTERN = "^\\d{1,3}-\\d{10}$";
+
+    public void setVehicleRegistrationNumber(String vehicleRegistrationNumber) {
+        // Validate license number before setting the value
+        if (validateLicenseNumber(vehicleRegistrationNumber)) {
+            this.vehicleRegistrationNumber = vehicleRegistrationNumber;
+        } else {
+            throw new IllegalArgumentException("Invalid license number format");
+        }
+    }
+
+    public void setBookedByUser(String bookedByUser) {
+        // Validate phone number before setting the value
+        if (validatePhoneNumber(bookedByUser)) {
+            this.bookedByUser = bookedByUser;
+        } else {
+            throw new IllegalArgumentException("Invalid phone number format");
+        }
+    }
+
+    private boolean validateLicenseNumber(String licenseNumber) {
+        // Check if the license number matches the pattern
+        return Pattern.matches(LICENSE_NUMBER_PATTERN, licenseNumber);
+    }
+
+    private boolean validatePhoneNumber(String phoneNumber) {
+        // Check if the phone number matches the pattern
+        return Pattern.matches(PHONE_NUMBER_PATTERN, phoneNumber);
+    }
 
     @Override
     public String toString() {
