ee290320447a0b4860a0b4816b9244b8082c410c
2222760 <rajdeep.dey@cognizant.com>
2023-05-24 13:04:01 +0530

    Phone validation, Issue #9 resolved

--- src/main/java/com/cts/dto/UserRegisterDto.java
+++ src/main/java/com/cts/dto/UserRegisterDto.java
@@ -2,13 +2,19 @@ package com.cts.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 
 @Getter
 @Setter
 public class UserRegisterDto {
     private String name;
     private String email;
+    @Pattern(regexp = "\\d{2}-\\d{10}", message = "Phone number must be in the format (country-code)-(number)")
     private String phno;
+    @Size(min = 15, max = 15, message = "License number must be 15 characters long")
+    @Pattern(regexp = "[A-Z]{2}_[A-Z0-9]{2}-\\w{9}", message = "License number must be in the format 2-character-state-code_RTO_Code-(License no given by RTO)")
     private String licenseno;
     private String address;
     private String password;
