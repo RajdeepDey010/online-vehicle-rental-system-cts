674b9d3e90386243f09b0caf7624bde81a023618
2222575 <bachu.sowmyasree@cognizant.com>
2023-05-24 15:18:36 +0530

    Added validations,Issue #6 resolved

--- src/main/java/com/cts/model/BookedSlot.java
+++ src/main/java/com/cts/model/BookedSlot.java
@@ -13,4 +13,35 @@ public class BookedSlot {
     private Date startTime;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
     private Date endTime;
+    Date d=new Date();  
+    private int year=d.getYear();
+
+    private static final int COMPANY_ESTABLISHMENT_YEAR = 2011;
+
+    public void setEndTime(Date endTime) {
+        // Validate that endTime is greater than startTime
+        if (endTime.before(startTime)) {
+            System.out.println("endTime must be greater than startTime");
+        } else {
+            this.endTime = endTime;
+        }
+    }
+
+    public void setStartTime(Date startTime) {
+        // Validate that MM is less than 12
+        if (startTime.getMonth() >= 12) {
+            System.out.println("MM must be less than 12");
+        } else {
+            this.startTime = startTime;
+        }
+    }
+
+    public void setYear(int year) {
+        // Validate that year is more than the year of establishment of the company
+        if (year <= COMPANY_ESTABLISHMENT_YEAR) {
+            System.out.println("Year must be more than " + COMPANY_ESTABLISHMENT_YEAR);
+        } else {
+            this.year = year;
+        }
+    }
 }
